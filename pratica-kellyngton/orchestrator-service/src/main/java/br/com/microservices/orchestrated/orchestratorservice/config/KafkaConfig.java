91ff3e2e0325cf11b17471226c4753bf9f59e190
kellyngton <kelldias895@gmail.com>
2024-02-13 09:09:14 -0300

    ORDER: commitando template de configuração do kafka

---
+++ pratica-kellyngton/orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/config/KafkaConfig.java
@@ -0,0 +1,59 @@
+package br.com.microservices.orchestrated.orchestratorservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+@RequiredArgsConstructor
+public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+    @Value("${spring.kafka.consumer.auto-offset-reset}")
+    private String autoOffsetReset;
+
+    @Bean
+    public DefaultKafkaProducerFactory<String, String> producerFactory(){
+        return new DefaultKafkaProducerFactory<String, String>(producerConfig());
+    }
+
+    private Map<String, Object> producerConfig(){
+        var configProps = new HashMap<String, Object>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    return configProps;
+    }
+
+    @Bean
+    public DefaultKafkaConsumerFactory<String, String> consumerFactory(){
+        return new DefaultKafkaConsumerFactory<String, String>(consumerConfig());
+    }
+
+    private Map<String, Object> consumerConfig(){
+        var configProps = new HashMap<String, Object>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
+        return configProps;
+    }
+}
