b17a3091b2bc93d865561be6c088602cfb548c12
kellyngton <kelldias895@gmail.com>
2024-02-13 10:19:54 -0300

    ORCHESTRATOR: adicionando kafka Template e tópicos necessários.

--- pratica-kellyngton/orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/config/KafkaConfig.java
+++ pratica-kellyngton/orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/config/KafkaConfig.java
@@ -1,6 +1,7 @@
 package br.com.microservices.orchestrated.orchestratorservice.config;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -9,8 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,8 +30,23 @@ public class KafkaConfig {
     @Value("${spring.kafka.consumer.auto-offset-reset}")
     private String autoOffsetReset;
 
+    @Value("${spring.kafka.topic.orchestrator}")
+    private String orchestratorTopic;
+
+    @Value("${spring.kafka.topic.finish-success}")
+    private String finishSuccessTopic;
+
+    @Value("${spring.kafka.topic.finish-fail}")
+    private String finishFailTopic;
+
+    @Value("${spring.kafka.topic.notify-ending}")
+    private String notifyEndingTopic;
+
+    private static final Integer REPLICA_COUNT = 1;
+    private static final Integer PARTITION_COUNT = 1;
+
     @Bean
-    public DefaultKafkaProducerFactory<String, String> producerFactory(){
+    public ProducerFactory<String, String> producerFactory(){
         return new DefaultKafkaProducerFactory<String, String>(producerConfig());
     }
 
@@ -43,7 +59,7 @@ public class KafkaConfig {
     }
 
     @Bean
-    public DefaultKafkaConsumerFactory<String, String> consumerFactory(){
+    public ConsumerFactory<String, String> consumerFactory(){
         return new DefaultKafkaConsumerFactory<String, String>(consumerConfig());
     }
 
@@ -55,5 +71,38 @@ public class KafkaConfig {
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
         return configProps;
+    }
+
+    @Bean
+    public KafkaTemplate kafkaTemplate(){
+        return new KafkaTemplate(producerFactory());
+    }
+
+    private NewTopic topicBuilder(String name){
+        return TopicBuilder
+                .name(name)
+                .replicas(REPLICA_COUNT)
+                .partitions(PARTITION_COUNT)
+                .build();
+    }
+
+    @Bean
+    public NewTopic finishSuccess(){
+        return  topicBuilder(finishSuccessTopic);
+    }
+
+    @Bean
+    public NewTopic finishFail(){
+        return  topicBuilder(finishFailTopic);
+    }
+
+    @Bean
+    public NewTopic orchestrator(){
+        return  topicBuilder(orchestratorTopic);
+    }
+
+    @Bean
+    public NewTopic notifyEnding(){
+        return  topicBuilder(notifyEndingTopic);
     }
 }
